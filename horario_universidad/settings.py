cc6eb54615bf4367dec67f6441b10b4fe5009b72
Vicente <vicentepa.canio@gmail.com>
2025-10-21 22:09:21 -0300

    Seleccion de sede

--- horario_universidad/settings.py
+++ horario_universidad/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bvkxxu_-s=w5f2q(-xaqq8n#1*0+(gy&t73ifwyyzbv33&w576'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'coolify.local:8001', 'coolify.local:9000', '.trycloudflare.com']
 
